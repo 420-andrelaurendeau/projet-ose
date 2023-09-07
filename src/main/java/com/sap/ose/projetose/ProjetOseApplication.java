50bc0c20ab2ab97caacf5b14ac92660862573005
PureLeaf <volteczx8@gmail.com>
2023-09-07 15:40:57 -0400

    Projet lancer

---
+++ src/main/java/com/sap/ose/projetose/ProjetOseApplication.java
@@ -0,0 +1,13 @@
+package com.sap.ose.projetose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetOseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetOseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sap/ose/projetose/ProjetOseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sap.ose.projetose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetOseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
