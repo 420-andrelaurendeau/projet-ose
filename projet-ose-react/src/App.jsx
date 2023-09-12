325f7d03dbdec773c13545626774a226429c7432
Robin M <robin59530@gmail.com>
2023-09-12 13:11:31 -0400

    Setup I18N

--- projet-ose-react/src/App.jsx
+++ projet-ose-react/src/App.jsx
@@ -1,13 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+import './i18n';
+
+const lngs = {
+  en: { nativeName: 'English' },
+  fr: { nativeName: 'Français' }
+};
 
 function App() {
+  const { t, i18n } = useTranslation();
+  
   return (
-    <div className="">
-      <h1 className="text-orange">Bienvenue à React avec TailwindCSS!</h1>
-      <h1 className="text-blue">Bienvenue à React avec TailwindCSS!</h1>
+    <>
 
-    </div>
+      {/**
+       * 
+       * Permet de tester le changement de langue
+       * 
+       * 
+       <header>
+          <div>
+            {Object.keys(lngs).map((lng) => (
+              <button key={lng} style={{ fontWeight: i18n.resolvedLanguage === lng ? 'bold' : 'normal' }} type="submit" onClick={() => i18n.changeLanguage(lng)}>
+                {lngs[lng].nativeName}
+              </button>
+            ))}
+          </div>
+        </header>
+      */}
+
+      <div className="">
+        
+        <h1 className="text-orange">Bienvenue à React avec TailwindCSS!</h1>
+        <h1 className="text-blue">Bienvenue à React avec TailwindCSS!</h1>
+
+      </div>
+    </>
   );
 }
 
