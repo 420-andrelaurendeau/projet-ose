89bbbc8dab18db9656d611a548852374a6c8f6d6
Pono Marc <ponomarc1210@gmail.com>
2023-09-12 13:26:11 -0400

    EQ5-118 Front-End fix

--- projet-ose-react/src/components/AppBar.js
+++ projet-ose-react/src/components/AppBar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../Style.css";
 import { Button } from "@material-tailwind/react";
+import logo from "../internship.png";
 const NavBar = () => {
     return (
         <div>
@@ -9,7 +10,7 @@ const NavBar = () => {
                     <div className="Logo">
                         {" "}
                         <span role="img" aria-label="logo">
-              😏
+              <img src={logo} alt={"internship"} width={50}/>
             </span>{" "}
                     </div>
                     <div className="Title"> My Sweet App </div>
--- projet-ose-react/src/index.js
+++ projet-ose-react/src/index.js
@@ -1,28 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import ConnectPage from "./pages/ConnectPage";
-import {createBrowserRouter, Navigate, RouterProvider} from "react-router-dom";
 
-const router = createBrowserRouter([
-    {
-        path: "/connect",
-        element: <ConnectPage/>,
-    },
-    {
-        path: "/",
-        element: <Navigate to='/connect'/>,
-    },
-]);
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <App />
     </React.StrictMode>
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
