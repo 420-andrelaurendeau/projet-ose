a1bd1be23eb2e6f4d54f4f62a7dcb7b92727ced6
ᑮᑘᖆᓬᒸᘍᗗᖴ <61324450+PureLeafGreen@users.noreply.github.com>
2023-09-13 18:37:25 -0400

    ajout d'annontation sur le modele Etudiant

--- src/main/java/com/sap/ose/projetose/modeles/Etudiant.java
+++ src/main/java/com/sap/ose/projetose/modeles/Etudiant.java
@@ -2,8 +2,14 @@ package com.sap.ose.projetose.modeles;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Etudiant {
     private String nom;
     private String prenom;
