5c77fd32ccf40bf2aa03a7c3529a40ab09eafbed
pureleaf <volteczx8@gmail.com>
2023-09-13 12:51:13 -0400

    Form en composant controller

--- projet-ose-react/src/pages/etudiantInscription.js
+++ projet-ose-react/src/pages/etudiantInscription.js
@@ -1,21 +1,87 @@
-function etudiantInscription() {
-    return (
-        <div>
-            <h1>Inscription</h1>
-            <form>
-                <label>n° matricule</label>
-                <input type="text" name="numEtudiant" />
-                <label>nom</label>
-                <input type="text" name="nom" />
-                <label>prénom</label>
-                <input type="text" name="prenom" />
-                <label>courriel</label>
-                <input type="text" name="courriel" />
+import {Component, useState} from "react";
+import axios from "axios";
 
-                <button type="submit">s'inscrire</button>
+class EtudiantInscription extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            matricule: '',
+            nom: '',
+            prenom: '',
+            courriel: '',
+            telephonne: '',
+            errors: {
+                matricule: '',
+                nom: '',
+                prenom: '',
+                courriel: '',
+                telephonne: ''
+            }
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    //handle change for multiple input with different name
+    handleChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        console.log(name + " " + value);
+        this.setState({
+            [name]: value
+        });
+    }
+    handleSubmit(event) {
+        const {matricule, nom, prenom, courriel, telephonne} = this.state;
+        event.preventDefault();
+        alert(`Votre matricule est ${matricule} ${nom} ${prenom} ${courriel} ${telephonne}`)
+        axios.post('http://localhost:8080/etudiantInscription', {
+            matricule: matricule,
+            nom: nom,
+            prenom: prenom,
+            courriel: courriel,
+            telephonne: telephonne
+        }).then(r => {
+            console.log(r)
+        }).catch(e => {
+            console.log(e)
+        }
+        )
+    }
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <div>
+                    <h1>Inscription de l'etudiant</h1>
+                    <label>
+                        <input type="text" name={"matricule"} value={this.state.value} onChange={this.handleChange} />
+                    </label>
+                    <br></br>
+                    <label>
+                        Nom:
+                        <input type="text" name={"nom"} value={this.state.value} onChange={this.handleChange} />
+                    </label>
+                    <br></br>
+                    <label>
+                        Prenom:
+                        <input type="text" name={"prenom"} value={this.state.value} onChange={this.handleChange} />
+                    </label>
+                    <br></br>
+                    <label>
+                        Courriel:
+                        <input type="text" name={"courriel"} value={this.state.value} onChange={this.handleChange} />
+                    </label>
+                    <br></br>
+                    <label>
+                        telephonne:
+                        <input type="text" name={"telephonne"} value={this.state.value} onChange={this.handleChange} />
+                    </label>
+                    <br></br>
+                    <button type="submit">Inscription</button>
+                </div>
             </form>
-        </div>
-    )
+        )
+    }
 }
 
-export default etudiantInscription
+export default EtudiantInscription;
