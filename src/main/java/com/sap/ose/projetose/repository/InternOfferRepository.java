b7f5a20eae2e18e4f2fb9e3c7ab8f6010a56c45a
CaptainMehdi <mehdi.diyani@gmail.com>
2023-09-28 16:36:33 -0400

    InternOffers fonctionnel

--- src/main/java/com/sap/ose/projetose/repository/InternOfferRepository.java
+++ src/main/java/com/sap/ose/projetose/repository/InternOfferRepository.java
@@ -1,11 +1,23 @@
 package com.sap.ose.projetose.repository;
 
+import com.sap.ose.projetose.dto.InternOfferDto;
 import com.sap.ose.projetose.modeles.InternOffer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface InternOfferRepository extends JpaRepository<InternOffer, Long> {
+    @Query("select i from InternOffer i where i.state = com.sap.ose.projetose.modeles.State.ACCEPTED")
+    List<InternOffer> findAllApproved();
+
+    @Query("select i from InternOffer i where i.state = com.sap.ose.projetose.modeles.State.PENDING")
+    List<InternOffer> findAllPending();
+
+    @Query("select i from InternOffer i where i.state = com.sap.ose.projetose.modeles.State.DECLINED")
+    List<InternOffer> findAllDeclined();
 
 }
 
