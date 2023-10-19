048aa0b453bf32fe60e91ac99a9e5e24e3db6562
Pono Marc <ponomarc1210@gmail.com>
2023-10-19 14:09:13 -0400

    EQ5-105 Front-End

--- projet-ose-react/src/components/common/EtudiantStage.tsx
+++ projet-ose-react/src/components/common/EtudiantStage.tsx
@@ -93,7 +93,7 @@ function EtudiantStage() {
                                                         disabled={
                                                             appliedOffers.find((appliedOffer: AppliedOffers) => appliedOffer.appliedOffer.id === offer.id) != null
                                                         }
-                                                        className="w-full flex justify-center py-2 px-4 border border-gray dark:border-darkgray text-sm font-medium rounded-md text-white disabled:bg-gray bg-blue dark:bg-orange hover:bg-cyan-300 dark:hover:bg-amber-400 focus:outline-none focus:shadow-outline-blue active:bg-blue transition duration-150 ease-in-out"
+                                                        className="w-full flex justify-center py-2 px-4 border border-gray dark:border-darkgray text-sm font-medium rounded-md text-white disabled:bg-gray bg-blue dark:disabled:bg-gray dark:bg-orange disabled:hover:bg-gray dark:disabled:hover:bg-gray hover:bg-cyan-300 dark:hover:bg-amber-400 focus:outline-none focus:shadow-outline-blue active:bg-blue transition duration-150 ease-in-out"
                                                     >
                                                         {fields.stage.apply.text}
                                                     </button>
