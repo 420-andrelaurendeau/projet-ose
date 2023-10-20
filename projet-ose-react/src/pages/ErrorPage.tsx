1d48b2b9718c7a8c0ad193638e79d9ac09e64552
ᑮᑘᖆᓬᒸᘍᗗᖴ <61324450+PureLeafGreen@users.noreply.github.com>
2023-10-19 21:46:53 -0400

    Essai d'ajout du dark theme

--- projet-ose-react/src/pages/ErrorPage.tsx
+++ projet-ose-react/src/pages/ErrorPage.tsx
@@ -1,7 +1,15 @@
 import ErrorComponent from "../components/common/ErrorComponent";
+import {useState} from "react";
 
 export default function ErrorPage() {
+    const [darkMode, setDarkMode] = useState(false);
+    function toggleDarkMode() {
+        setDarkMode(!darkMode)
+    }
     return (
-        <ErrorComponent/>
+        <ErrorComponent
+            darkMode={darkMode}
+            toggleDarkMode={toggleDarkMode}
+        />
     );
 }
