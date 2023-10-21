1c1216e3d3cc2daefbdd31f586d88fc9c52c28b1
Marco Attia <54147992+Vaneixus@users.noreply.github.com>
2023-10-21 07:52:46 -0400

    Add Messages for Login Information

--- src/main/java/com/sap/ose/projetose/dtos/UserLoginInformationDto.java
+++ src/main/java/com/sap/ose/projetose/dtos/UserLoginInformationDto.java
@@ -1,5 +1,7 @@
 package com.sap.ose.projetose.dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +10,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserLoginInformationDto {
+    @NotBlank(message = "{user.email.notBlank}")
+    @Email(message = "{user.email.malformed}")
     private String email;
+    @NotBlank(message = "{user.password.notBlank}")
     private String password;
 }
