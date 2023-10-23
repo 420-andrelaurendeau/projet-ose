f7724485060d1e86de4e3ddd8861d46b3245d68f
Pono Marc <ponomarc1210@gmail.com>
2023-10-23 13:38:59 -0400

    refactorisation et bonne methode

--- projet-ose-react/src/router/EmployerRouter.tsx
+++ projet-ose-react/src/router/EmployerRouter.tsx
@@ -3,12 +3,25 @@ import {Route, Routes} from "react-router-dom";
 import ProtectedRoute from "./ProtectedRoute";
 import Layout from "../components/layout/Layout";
 import EtudiantStagePage from "../pages/EtudiantStagePage";
+import GSOffersPage from "../pages/internshipManager/Offers/GSOffersPage";
+import GSOfferPage from "../pages/internshipManager/Offer/GSOfferPage";
+import EmployeurHomePage from "../pages/EmployeurHomePage";
+import EmployeurOffer from "../components/common/EmployeurOffer";
 
 const EmployerRouter: React.FC = () => {
     return (
-        <Routes>
-
-        </Routes>
+        <ProtectedRoute requiredRoles={['employer']}>
+            <Routes>
+                <Route path="/" element={<Layout/>}>
+                    <Route index element={<Layout/>}/>
+                    <Route path="home" element={<EmployeurHomePage/>}>
+                        <Route path="offre" element={<EmployeurOffer/>}/>
+                        <Route path="" element={<GSOfferPage/>}/>
+                    </Route>
+                    <Route path="/offer/:id" element={<GSOfferPage/>} />
+                </Route>
+            </Routes>
+        </ProtectedRoute>
     );
 };
 
