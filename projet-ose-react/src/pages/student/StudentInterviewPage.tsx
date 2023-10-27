c82b5c06b283f3915498690d98ad54ea36f2e117
pureleaf <61324450+PureLeafGreen@users.noreply.github.com>
2023-10-26 22:29:33 -0400

    Commencement pour afficher la page des interview dun etudiant

--- projet-ose-react/src/pages/student/StudentInterviewPage.tsx
+++ projet-ose-react/src/pages/student/StudentInterviewPage.tsx
@@ -1,5 +1,37 @@
+import React, {useEffect, useRef, useState} from "react";
+import {fetchInterviews, fetchInterviewsCountForStudent} from "../../api/StudentApi";
+import {getUser} from "../../api/UtilisateurAPI";
+import {getStudentAppliedOffers, offresEtudiant} from "../../api/InterOfferJobAPI";
+import {useAuth} from "../../authentication/AuthContext";
+import {Interview} from "../../model/Interview";
+
 export default function StudentInterviewPage() {
-    //TODO : fetch interviews by studentId
+
+    const [user, setUser] = useState<any>(null);
+    const [interviewsNb, setInterviewsNb] = React.useState<Interview[]>([]);
+    const isLoading = useRef(false);
+    const auth = useAuth();
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            isLoading.current = true;
+
+            getUser(auth.userEmail!)
+                .then((resUser) => {
+                    setUser(resUser);
+                    console.log(resUser);
+                    fetchInterviews(resUser.id).then((res) => {
+                        setInterviewsNb(res);
+                        console.log(interviewsNb);
+                    });
+                })
+                .catch((err) => {
+                    console.log(err);
+                })
+                .finally(() => (isLoading.current = false));
+        };
+        if (!isLoading.current) fetchUser();
+    }, []);
     return (
         <div>
             <h1>StudentInterviewPage</h1>
