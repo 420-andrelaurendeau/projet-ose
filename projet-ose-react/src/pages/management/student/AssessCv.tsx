d4315a077638878c324a1297da6e32b7b19ca296
Marco Attia <54147992+Vaneixus@users.noreply.github.com>
2023-11-02 00:44:19 -0400

    Fix CV à Évaluer

--- projet-ose-react/src/pages/management/student/AssessCv.tsx
+++ projet-ose-react/src/pages/management/student/AssessCv.tsx
@@ -52,33 +52,33 @@ function EvaluerCV() {
 
     return (
         <>
-            <div className="md:p-16 p-4">
+            <div className="lg:p-16 p-4">
                 <h1 className="text-center font-extrabold text-2xl dark:text-gray">{fields.title}</h1>
                 {files.map((file) =>
                     <>
                         <div className="mx-12 my-16 px-7 py-3 bg-slate-50 hover:bg-slate-100 rounded-3xl flex flex-col md:flex-row flex-wrap dark:bg-dark">
-                            <div className="flex-item md:flex-row md:flex-wrap md:flex-grow">
-                                <p className="basis-full flex-grow pb-2 md:pb-0 dark:text-white">{file.etudiant?.nom}, {file.etudiant?.prenom}</p>
+                            <div className="flex-item lg:flex-row lg:flex-wrap md:flex-grow overflow-ellipsis pb-2">
+                                <p className="basis-full flex-grow pb-2 lg:pb-0 dark:text-white">{file.etudiant?.nom}, {file.etudiant?.prenom}</p>
                                 <p className="basis-full flex-grow dark:text-white">{file.etudiant?.matricule}</p>
                                 <p className="basis-full flex-grow dark:text-white">{file.etudiant?.email}</p>
                                 <p className="basis-full flex-grow dark:text-white">{file.fileName}</p>
                             </div>
-                            <div className="rounded bg-gray my-4 md:my-0 text-center sm:flex-grow md:flex-grow-0">
+                            <div className="md:mx-3 my-4 lg:my-0 text-center lg:flex-grow-0 pb-2">
                                 <button
-                                    className="text-blue-500 py-2 sm:px-4 lg:px-10 hover:text-blue-700 text-center text-white align-middle h-full w-full"
+                                    className="text-blue-500 rounded bg-gray py-2 sm:px-4 lg:px-10 hover:text-blue-700 text-center text-white align-middle h-full w-full"
                                     onClick={() => handleDownloadFile(file)}
                                 >
                                     <p className="dark:text-white">{fields.button.download}</p>
                                     <FontAwesomeIcon icon={faDownload} className="scale-150 dark:text-white" />
                                 </button>
                             </div>
-                            <div className="flex-grow my-4 md:my-0">
+                            <div className="md:flex-grow my-4 lg:my-0 pb-2">
                                 <div className="flex flex-col md:flex-row md:justify-end h-full w-full flex-wrap">
-                                    <button className="bg-green hover:bg-green-700 text-white font-bold py-2 rounded mb-3 md:px-4 lg:aspect-square md:h-full md:mr-4"
+                                    <button className="bg-green hover:bg-green-700 text-white font-bold py-2 rounded mb-3 md:mb-0 md:mr-1 md:px-2 md:aspect-square lg:h-full lg:mr-4"
                                             onClick={_ => ApproveFile(file)}>
                                         {fields.button.accept}
                                     </button>
-                                    <button className="bg-red hover:bg-red-700 text-white font-bold py-2 rounded md:px-4 lg:aspect-square md:h-full"
+                                    <button className="bg-red hover:bg-red-700 text-white font-bold py-2 rounded md:px-2 md:aspect-square lg:h-full"
                                             onClick={_ => DeclineFile(file)}>
                                         {fields.button.decline}
                                     </button>
