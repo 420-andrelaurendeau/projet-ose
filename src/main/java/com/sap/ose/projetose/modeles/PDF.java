e9352c620c8ef036095e07e24d0428b943ec3a5d
Robin <robin59530@gmail.com>
2023-11-11 16:36:47 -0500

    Ajout du model pdf

---
+++ src/main/java/com/sap/ose/projetose/modeles/PDF.java
@@ -0,0 +1,31 @@
+package com.sap.ose.projetose.modeles;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PDF {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+    LocalDate createdDate;
+    Boolean isActive;
+
+    @ManyToOne()
+    @JoinColumn(name = "file_id")
+    private File file;
+
+    public PDF(File file) {
+        this.file = file;
+        this.createdDate = LocalDate.now();
+        this.isActive = true;
+    }
+}
