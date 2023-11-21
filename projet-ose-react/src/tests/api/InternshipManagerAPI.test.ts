f62eec730178ce0b5d47ceb683c5b4f14e9463f3
Robin <robin59530@gmail.com>
2023-11-21 11:27:27 -0500

    Ajout test pour récupération des offres d emploi pour le GS

--- projet-ose-react/src/tests/api/InternshipManagerAPI.test.ts
+++ projet-ose-react/src/tests/api/InternshipManagerAPI.test.ts
@@ -32,16 +32,37 @@ Object.defineProperty(window, 'localStorage', {
 });
 
 describe('getInternshipOffers', () => {
+
     afterEach(() => {
-        api.get.mockReset();
+        (api.get as jest.Mock).mockReset();
         mockedLocalStorage.getItem.mockReset();
     });
 
     it('fetches internship offers successfully', async () => {
         const mockData = { data: 'some data' };
-        api.get.mockResolvedValue({ data: mockData });
+        (api.get as jest.Mock).mockResolvedValue({ data: mockData });
         mockedLocalStorage.getItem.mockReturnValue('fakeToken');
 
+        const params = {
+            page: 1,
+            size: 10,
+            state: 'active',
+            sortField: 'date',
+            sortDirection: 'desc',
+            session: 'autunm2023'
+        };
+
+        const result = await getIntershipOffers(params);
+
+        expect(api.get).toHaveBeenCalledWith('internshipManager/offers', expect.anything());
+        expect(result).toEqual(mockData);
+    });
+
+    it('sends the correct parameters to the API', async () => {
+        // Simuler une réponse (peu importe les données dans ce cas)
+        (api.get as jest.Mock).mockResolvedValue({ data: {} });
+
+        // Paramètres pour appeler getInternshipOffers
         const params = {
             page: 1,
             size: 10,
@@ -51,9 +72,42 @@ describe('getInternshipOffers', () => {
             session: 'sessionToken'
         };
 
-        const result = await getIntershipOffers(params);
+        // Appeler la fonction
+        await getIntershipOffers(params);
 
-        expect(api.get).toHaveBeenCalledWith('internshipManager/offers', expect.anything());
-        expect(result).toEqual(mockData);
+        // Vérifier si les bons paramètres sont passés à l'API
+        expect(api.get).toHaveBeenCalledWith(
+            'internshipManager/offers',
+            expect.objectContaining({
+                params: {
+                    page: 1,
+                    size: 10,
+                    state: 'active',
+                    sortField: 'date',
+                    sortDirection: 'desc',
+                    session: 'sessionToken'
+                }
+            })
+        );
     });
+
+    it('throws an error when the API call fails', async () => {
+        // Simuler une erreur
+        const errorMessage = 'Network error';
+        (api.get as jest.Mock).mockRejectedValue(new Error(errorMessage));
+
+        // Paramètres pour appeler getInternshipOffers
+        const params = {
+            page: 1,
+            size: 10,
+            state: 'active',
+            sortField: 'date',
+            sortDirection: 'desc',
+            session: 'sessionToken'
+        };
+
+        // S'attendre à ce que la fonction lance une erreur
+        await expect(getIntershipOffers(params)).rejects.toThrow(errorMessage);
+    });
+
 });
