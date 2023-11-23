d4ce9231f7b6c9bedea76c7d347cd5cae0ff0ebf
Marco Attia <54147992+Vaneixus@users.noreply.github.com>
2023-11-22 23:26:15 -0500

    Add basic structure.

--- projet-ose-react/src/components/common/shared/messaging/MessageBox.tsx
+++ projet-ose-react/src/components/common/shared/messaging/MessageBox.tsx
@@ -1,7 +1,20 @@
-const MessagePage = () => {
-    return <>
+import {Message} from "../../../../model/Message";
 
-    </>;
+const MessagePage = () => {
+    function getMessageHeaders() {
+        return [{
+            id: 0,
+            title: "Hello World",
+        }] as Message[];
+    }
+
+    return <div className="sr-only">
+        {
+            getMessageHeaders().map(header =>
+                <p>{header.title}</p>
+            )
+        }
+    </div>;
 }
 
 export default MessagePage;
