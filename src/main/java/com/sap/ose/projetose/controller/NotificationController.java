bbcdfa0cd5a31e6f71815882a42117b69071bb6e
CaptainMehdi <mehdi.diyani@gmail.com>
2023-11-26 21:41:39 -0500

    notif controller fait

---
+++ src/main/java/com/sap/ose/projetose/controller/NotificationController.java
@@ -0,0 +1,29 @@
+package com.sap.ose.projetose.controller;
+
+import com.sap.ose.projetose.dto.NotificationDto;
+import com.sap.ose.projetose.dto.ProgrammeDto;
+import com.sap.ose.projetose.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/notifications")
+@CrossOrigin(origins = "http://localhost:3000")
+@RequiredArgsConstructor
+public class NotificationController {
+
+    private final NotificationService notificationService;
+
+
+    @GetMapping("/{id}")
+    @CrossOrigin(origins = "http://localhost:3000")
+    @PreAuthorize("hasAuthority('internshipmanager') OR (hasAuthority('student')) OR (hasAuthority('employer'))")
+    public List<NotificationDto> getNotifications(@PathVariable Long user_id) {
+        return notificationService.getNotificationByUserId(user_id);
+    }
+
+}
