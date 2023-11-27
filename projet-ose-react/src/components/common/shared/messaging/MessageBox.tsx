599032057b7b6508e3ad298133d217f1975a5249
Marco Attia <54147992+Vaneixus@users.noreply.github.com>
2023-11-27 14:07:57 -0500

    Allow list to overflow.

--- projet-ose-react/src/components/common/shared/messaging/MessageBox.tsx
+++ projet-ose-react/src/components/common/shared/messaging/MessageBox.tsx
@@ -10,7 +10,7 @@ const MessageBox: React.FC<MessageBoxProps> = (props) => {
     const {t} = useTranslation();
 
     return props.messages.length > 0
-        ? <div className="z-50 min-w-[320px] max-w-[400px] min-h-[240px] rounded-b-md absolute right-[calc(100%-1.5rem)] bg-neutral-50 border-l-1 border-b-1">
+        ? <div className="z-50 min-w-[320px] max-w-[400px] min-h-[240px] max-h-[240px] overflow-y-auto rounded-b-md absolute right-[calc(100%-1.5rem)] bg-neutral-50 border-l-1 border-b-1">
                 {
                     props.messages.map(header =>
                         <>
