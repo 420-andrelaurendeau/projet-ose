cdc2f0f9e9465d29e8b5e91e5d1d07e324e9bb5a
Marco Attia <54147992+Vaneixus@users.noreply.github.com>
2023-11-27 14:07:41 -0500

    Fix notifications order.

--- src/main/java/com/sap/ose/projetose/repository/NotificationRepository.java
+++ src/main/java/com/sap/ose/projetose/repository/NotificationRepository.java
@@ -2,14 +2,12 @@ package com.sap.ose.projetose.repository;
 
 import com.sap.ose.projetose.modeles.Notifications;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Stack;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notifications,Long> {
-
-    @Query("SELECT n FROM Notifications n WHERE n.receveurs.id = ?1")
-    List<Notifications> findByReceveurs_id(long id);
+    
+    Stack<Notifications> findByReceveurs_idOrderByIdDesc(long id);
 }
