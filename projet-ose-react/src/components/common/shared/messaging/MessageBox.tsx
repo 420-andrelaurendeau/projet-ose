a5a2004370a705733e201eb5e91e04f1bc651f23
Marco Attia <54147992+Vaneixus@users.noreply.github.com>
2023-11-28 18:52:14 -0500

    Partial: Redirection to appropriate pages.

--- projet-ose-react/src/components/common/shared/messaging/MessageBox.tsx
+++ projet-ose-react/src/components/common/shared/messaging/MessageBox.tsx
@@ -9,17 +9,22 @@ type MessageBoxProps = {
 const MessageBox: React.FC<MessageBoxProps> = (props) => {
     const {t} = useTranslation();
 
+    function handleClick() {
+
+    }
+
     return props.messages.length > 0
-        ? <div className="z-50 min-w-[320px] max-w-[400px] min-h-[240px] max-h-[240px] overflow-y-auto rounded-b-md absolute right-[calc(100%-1.5rem)] bg-neutral-50 border-l-1 border-b-1">
+        ? <div className="z-50 min-w-[320px] max-w-[400px] min-h-[240px] max-h-[240px] overflow-y-auto rounded-b-md absolute right-[calc(100%-1.5rem)] bg-neutral-50 border-l-1 border-b-1 last:border-b-0">
                 {
                     props.messages.map(header =>
                         <>
-                            <p className={"px-4 py-2 text-ellipsis whitespace-nowrap overflow-hidden"
+                            <a className={"px-4 py-2 text-ellipsis whitespace-nowrap overflow-hidden"
                                 + (header.isRead
                                     ? " bg-neutral-200"
-                                    : "")}>
+                                    : "")}
+                                onClick={handleClick}>
                                 {t(header.message)}
-                            </p>
+                            </a>
                             <hr/>
                         </>
                     )
---
+++ projet-ose-react/src/model/NotificationLinkMap.ts
@@ -0,0 +1,19 @@
+export default class NotificationLinkMap {
+    array = {
+        "FormFields.notifications.newOfferAvailable": "",
+        "FormFields.notifications.contractText": "",
+        "FormFields.notifications.stageText": "",
+        "FormFields.notifications.cvAccepter": "",
+        "FormFields.notifications.cvRefuser": "",
+        "FormFields.notifications.studentApplyOnOffer": "",
+        "FormFields.notifications.revueCv": "",
+        "FormFields.notifications.youAreAcceptedForStage": "",
+        "FormFields.notifications.newOfferSavedByEmployeur": "",
+        "FormFields.notifications.contractSignedByGS": "",
+        "FormFields.notifications.newContractAsBeenCreated": "",
+        "FormFields.notifications.contractAsBeenSignedByThreeParties": "",
+        "FormFields.notifications.offerAsBeenAccpeted": "",
+        "FormFields.notifications.offerAsBeenDeclined": "",
+        "FormFields.notifications.newStageAsBeenCreated": ""
+    }
+}
