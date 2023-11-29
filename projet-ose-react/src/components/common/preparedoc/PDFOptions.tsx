e3f1bd63c788d511cf72234e65aa1e7a5a7a4ac2
pureleaf <61324450+PureLeafGreen@users.noreply.github.com>
2023-11-28 19:02:57 -0500

    Bouton comme les autres

--- projet-ose-react/src/components/common/preparedoc/PDFOptions.tsx
+++ projet-ose-react/src/components/common/preparedoc/PDFOptions.tsx
@@ -16,7 +16,7 @@ function PDFOptions(props: any) {
 
         <button
             type="button"
-            className="inline-flex px-4 py-2 border border-transparent hover:border-black dark:border-white shadow-sm text-sm font-medium rounded-md text-neutral-900 bg-white hover:bg-neutral-50 dark:bg-dark dark:hover:bg-black dark:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-neutral-500"
+            className="inline-flex items-center px-4 py-2 border border-transparent hover:border-black shadow-sm text-sm font-medium rounded-md text-white bg-red hover:bg-rose-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-neutral-500"
             onClick={() => navigate(`/${userRole}/home/internshipagreement/` + props.contractId)}
         >
             {t("Shared.ReturnButton.text")} <Icon className="w-5 h-5 fill-current hover:font-bold"/>
