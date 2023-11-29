11d62491a7e77aef60549b485581fd10668cb92f
pureleaf <61324450+PureLeafGreen@users.noreply.github.com>
2023-11-28 20:39:54 -0500

    Ecriture illisible

--- projet-ose-react/src/pages/management/student/AssessCv.tsx
+++ projet-ose-react/src/pages/management/student/AssessCv.tsx
@@ -77,7 +77,7 @@ function EvaluerCV() {
                             </div>
                             <div className="md:mx-3 my-4 lg:my-0 text-center lg:flex-grow-0 pb-2">
                                 <button
-                                    className="text-blue-500 rounded bg-gray py-2 sm:px-4 lg:px-10 hover:text-blue-700 text-center align-middle h-full w-full"
+                                    className="text-blue-900 rounded bg-gray py-2 sm:px-4 lg:px-10 hover:text-blue-700 text-center align-middle h-full w-full"
                                     onClick={() => handleDownloadFile(file)}
                                 >
                                     <p className="dark:text-white">{t("StudentCvEvaluation.button.download")}</p>
