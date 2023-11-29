5cbc68645095d67c38d575884a195ff7107a6897
Marco Attia <54147992+Vaneixus@users.noreply.github.com>
2023-11-28 20:44:35 -0500

    Link map for notifications.

--- projet-ose-react/src/model/NotificationLinkMap.ts
+++ projet-ose-react/src/model/NotificationLinkMap.ts
@@ -1,19 +1,19 @@
 export default class NotificationLinkMap {
-    array = {
+    static array: Record<string, string> = {
         "FormFields.notifications.newOfferAvailable": "",
         "FormFields.notifications.contractText": "",
         "FormFields.notifications.stageText": "",
-        "FormFields.notifications.cvAccepter": "",
-        "FormFields.notifications.cvRefuser": "",
+        "FormFields.notifications.cvAccepter": "/student/home/cv",
+        "FormFields.notifications.cvRefuser": "/student/home/cv",
         "FormFields.notifications.studentApplyOnOffer": "",
-        "FormFields.notifications.revueCv": "",
-        "FormFields.notifications.youAreAcceptedForStage": "",
-        "FormFields.notifications.newOfferSavedByEmployeur": "",
+        "FormFields.notifications.revueCv": "/internshipmanager/home/studentCvReview",
+        "FormFields.notifications.youAreAcceptedForStage": "/student/home/stage",
+        "FormFields.notifications.newOfferSavedByEmployeur": "/internshipmanager/home/offers",
         "FormFields.notifications.contractSignedByGS": "",
-        "FormFields.notifications.newContractAsBeenCreated": "",
-        "FormFields.notifications.contractAsBeenSignedByThreeParties": "",
+        "FormFields.notifications.newContractAsBeenCreated": "internshipmanager/home/internshipagreement",
+        "FormFields.notifications.contractAsBeenSignedByThreeParties": "internshipmanager/home/internshipagreement",
         "FormFields.notifications.offerAsBeenAccpeted": "",
         "FormFields.notifications.offerAsBeenDeclined": "",
-        "FormFields.notifications.newStageAsBeenCreated": ""
+        "FormFields.notifications.newStageAsBeenCreated": "internshipmanager/home/"
     }
 }
