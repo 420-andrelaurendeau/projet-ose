23e7d121eb3a5cd350b10e434fac2166f1dbf7e5
Marco Attia <54147992+Vaneixus@users.noreply.github.com>
2023-11-28 21:26:19 -0500

    Fix notifications display.

--- projet-ose-react/src/components/common/shared/messaging/MessageBox.tsx
+++ projet-ose-react/src/components/common/shared/messaging/MessageBox.tsx
@@ -22,19 +22,17 @@ const MessageBox: React.FC<MessageBoxProps> = (props) => {
     }
 
     return props.messages.length > 0
-        ? <div className="z-50 min-w-[320px] max-w-[400px] min-h-[240px] max-h-[240px] overflow-y-auto rounded-b-md absolute right-[calc(100%-1.5rem)] bg-neutral-50 border-l-1 border-b-1 last:border-b-0">
+        ? <div className="z-50 min-w-[320px] max-w-[400px] max-h-[240px] overflow-y-auto rounded-b-md absolute right-[calc(100%-1.5rem)] bg-neutral-50">
                 {
                     props.messages.map(header =>
-                        <>
-                            <a className={"px-4 py-2 text-ellipsis whitespace-nowrap overflow-hidden"
+                            <a  key={header.id}
+                                className={"block text-ellipsis whitespace-nowrap overflow-hidden px-4 py-2 w-full border-b last:border-b-0 "
                                 + (header.isRead
                                     ? " bg-neutral-200"
                                     : "")}
                                 onClick={event => handleClick(header)}>
                                 {t(header.message)}
                             </a>
-                            <hr/>
-                        </>
                     )
                 }
             </div>
