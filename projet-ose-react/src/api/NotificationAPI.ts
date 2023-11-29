fd59c4cb5b7ee8ba4d0541c96e8953c3625a1dad
Marco Attia <54147992+Vaneixus@users.noreply.github.com>
2023-11-28 20:44:07 -0500

    Fix API call for notifications

--- projet-ose-react/src/api/NotificationAPI.ts
+++ projet-ose-react/src/api/NotificationAPI.ts
@@ -3,7 +3,7 @@ import {Message} from "../model/Message";
 
 export const fetchUserNotifications = async (user_id: number ): Promise<Message[]> => {
     try {
-        const res = await api.get(`notifications/`+ user_id)
+        const res = await api.get("notifications/" + user_id)
         console.log(res.data)
         return res.data
     }
