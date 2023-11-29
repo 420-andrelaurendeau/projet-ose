607570c693cb43891bdabd5ec65e72edf25ec7a5
Marco Attia <54147992+Vaneixus@users.noreply.github.com>
2023-11-29 18:38:22 -0500

    Create Linkmap for notifications

--- projet-ose-react/src/model/NotificationLinkMap.ts
+++ projet-ose-react/src/model/NotificationLinkMap.ts
@@ -1,19 +1,22 @@
 export default class NotificationLinkMap {
     static array: Record<string, string> = {
-        "formField.notifications.newOfferAvailable": "",
-        "formField.notifications.contractText": "",
-        "formField.notifications.stageText": "",
+        "formField.notifications.newOfferAvailable": "/student/home/offers",
         "formField.notifications.cvAccepter": "/student/home/cv",
         "formField.notifications.cvRefuser": "/student/home/cv",
-        "formField.notifications.studentApplyOnOffer": "",
+        "formField.notifications.studentApplyOnOffer": "/employer/home/offers",
         "formField.notifications.revueCv": "/internshipmanager/home/studentCvReview",
         "formField.notifications.youAreAcceptedForStage": "/student/home/stage",
         "formField.notifications.newOfferSavedByEmployeur": "/internshipmanager/home/offers",
-        "formField.notifications.contractSignedByGS": "",
-        "formField.notifications.newContractAsBeenCreated": "internshipmanager/home/internshipagreement",
-        "formField.notifications.contractAsBeenSignedByThreeParties": "internshipmanager/home/internshipagreement",
-        "formField.notifications.offerAsBeenAccpeted": "",
-        "formField.notifications.offerAsBeenDeclined": "",
-        "formField.notifications.newStageAsBeenCreated": "internshipmanager/home/"
+        "FormFields.notifications.contractSignedByGSForEmployer": "/employer/home/internshipagreement",
+        "FormFields.notifications.contractSignedByGSForStudent": "/student/home/internshipagreement",
+        "FormFields.notifications.contractSignedByStudentForEmployer": "/employer/home/internshipagreement",
+        "FormFields.notifications.contractSignedByStudentForGS": "/internshipmanager/home/internshipagreement",
+        "FormFields.notifications.contractSignedByEmployerForStudent": "/student/home/internshipagreement",
+        "FormFields.notifications.contractSignedByEmployerForGS": "/internshipmanager/home/internshipagreement",
+        "formField.notifications.newContractAsBeenCreated": "/internshipmanager/home/internshipagreement",
+        "formField.notifications.contractAsBeenSignedByThreeParties": "/internshipmanager/home/internshipagreement",
+        "formField.notifications.offerAsBeenAccpeted": "/employer/home/offers",
+        "formField.notifications.offerAsBeenDeclined": "/employer/home/offers",
+        "formField.notifications.newStageAsBeenCreated": "/internshipmanager/home"
     }
 }
