35bdd7af45c0a8b54e4d68effad4f7a3f1d33e19
Marco Attia <54147992+Vaneixus@users.noreply.github.com>
2023-11-29 20:44:01 -0500

    Mail service is optional

--- src/main/java/com/sap/ose/projetose/service/EmailService.java
+++ src/main/java/com/sap/ose/projetose/service/EmailService.java
@@ -1,6 +1,7 @@
 package com.sap.ose.projetose.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -8,10 +9,15 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class EmailService {
+    @Value("${sendEmails}")
+    private Boolean enabled = false;
 
     private final JavaMailSender mailSender;
 
     public void sendEmail(String to, String subject, String body) {
+        if (!enabled) {
+            return;
+        }
         SimpleMailMessage message = new SimpleMailMessage();
 
         message.setTo(to);
