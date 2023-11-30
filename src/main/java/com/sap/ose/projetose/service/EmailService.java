42f87801953e6577e118f2478721c794bcdab1fe
Marco Attia <54147992+Vaneixus@users.noreply.github.com>
2023-11-29 20:12:43 -0500

    cleanup

--- src/main/java/com/sap/ose/projetose/service/EmailService.java
+++ src/main/java/com/sap/ose/projetose/service/EmailService.java
@@ -1,7 +1,6 @@
 package com.sap.ose.projetose.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
