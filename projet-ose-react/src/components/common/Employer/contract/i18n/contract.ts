0b48814055236f8af6d197f93bd2f63e4c54202b
Marco Attia <54147992+Vaneixus@users.noreply.github.com>
2023-11-30 09:39:11 -0500

    fix i18n

--- projet-ose-react/src/components/common/Employer/contract/i18n/contract.ts
+++ projet-ose-react/src/components/common/Employer/contract/i18n/contract.ts
@@ -37,7 +37,7 @@ const contractPage = {
             },
             ACCEPTED: {
                 name: 'APPROVED',
-                text: 'Approved'
+                text: 'Accepted'
             },
             DECLINED: {
                 name: 'DECLINED',
